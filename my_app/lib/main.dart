7fa83c851c9ed43e44f3b656492bff5adc11dbb6
girishm865 <gj990206@gmail.com>
2025-03-27 15:40:26 +0530

    Update main.dart

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,4 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Free Fire Tournament'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            'Welcome to Free Fire Tournament!',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
