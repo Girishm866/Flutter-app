72e4e715ec45cbb5fef4797981291c49ef1c1c45
girishm865 <gj990206@gmail.com>
2025-03-27 18:05:05 +0530

    Update main.dart

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,4 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirestoreAddDataScreen(),
+    );
+  }
+}
+
+class FirestoreAddDataScreen extends StatefulWidget {
+  @override
+  _FirestoreAddDataScreenState createState() => _FirestoreAddDataScreenState();
+}
+
+class _FirestoreAddDataScreenState extends State<FirestoreAddDataScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+
+  void addUserData() {
+    FirebaseFirestore.instance.collection('users').add({
+      'name': nameController.text,
+      'email': emailController.text,
+      'createdAt': Timestamp.now(),
+    }).then((value) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Data Added Successfully!')),
+      );
+      nameController.clear();
+      emailController.clear();
+    }).catchError((error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to Add Data: $error')),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Data to Firestore')),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Enter Name'),
+            ),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Enter Email'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: addUserData,
+              child: Text('Add Data'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
